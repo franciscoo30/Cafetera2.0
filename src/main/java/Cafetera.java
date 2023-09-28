105351fb9171ffc90646f5f6dd38cc0e9b283f08
diagnostico <f.barrozo02@ufromail.cl>
2023-09-28 11:44:23 -0300

    Commit Código

---
+++ src/main/java/Cafetera.java
@@ -0,0 +1,18 @@
+public class Cafetera {
+    String marca;
+    String color;
+    int capacidadMaxima;
+    int capacidadActual;
+    public void llenarCafetera(){
+
+    }
+    public void servirTaza(){
+
+    }
+    public void vaciarCafetera(){
+
+    }
+    public void agregarCafe(){
+
+    }
+}
